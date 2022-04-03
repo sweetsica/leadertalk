9f2222aa3cea8fd2b6cca7f026dc2830c70955dc
sweetsica <barondeepsnow@gmail.com>
2022-04-04 01:16:35 +0700

    fix title

--- resources/views/thankyou.blade.php
+++ resources/views/thankyou.blade.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1">
-    <title>Gymove - Fitness Bootstrap Admin Dashboard</title>
+    <title>Leader Talk Dashboard</title>
     <!-- Favicon icon -->
     <link rel="icon" type="image/png" sizes="16x16" href="{{asset('admin/images/favicon.png')}}">
     <link href="{{asset('admin/css/style.css')}}" rel="stylesheet">
